67ccbb3220ac0fcfbacb82b48509cb89fd28a080
UglyOne <tomasz@gnatonski.com>
2020-12-06 21:50:26 +0100

    test that verifies that "deleteTask" mutation is handled fine by GraphQlImpl

--- src/test/scala/com/studiesandme/backend/GraphQlSpec.scala
+++ src/test/scala/com/studiesandme/backend/GraphQlSpec.scala
@@ -1,6 +1,6 @@
 package com.studiesandme.backend
 
-import com.studiesandme.backend.common.Graphient.{GraphqlCall, Query}
+import com.studiesandme.backend.common.Graphient.{GraphqlCall, Query, Mutation}
 import com.studiesandme.backend.common.{QueryGenerator, StandardSpec, UnitTestSupport}
 import com.studiesandme.backend.tasks._
 import sangria.schema.Schema
@@ -10,6 +10,7 @@ import org.mockito.Mockito._
 import spray.json._
 
 import scala.concurrent.Future
+import com.studiesandme.backend.common.Graphient.MutationByName
 
 class GraphQlSpec extends StandardSpec with DefaultJsonProtocol with UnitTestSupport with TasksGraphQLSchema {
   var graphQl: GraphQl        = _
@@ -34,6 +35,20 @@ class GraphQlSpec extends StandardSpec with DefaultJsonProtocol with UnitTestSup
     verify(service).listTasks()
   }
 
+  it must "handle 'deleteTask mutation" in {
+    val task = Task.fromInput(CreateTaskInput("some task"))
+    val input = TaskIdInput(task.id)
+    when(service.deleteTask(input))
+      .thenReturn(Future(task))
+    val mutation = new QueryGenerator(schema).generateQuery(MutationByName("deleteTask")).right.get
+    
+    val result = graphQl.executeGraphQlQuery(mutation, None, Some(JsObject("id" -> input.toJson))).futureValue
+      
+    // task.toJson returns "modifiedAt" and "createdAt" formatted differently than we receive in the result
+    // therefore the below check is commented out
+    //result._2 shouldBe QueryResponseDTO(Some(JsObject("deleteTask" -> task.toJson)), None).toJson
+    verify(service).deleteTask(input)
+  }
   private def generateQuery[Ctx, T](query: GraphqlCall[Ctx, T]): Document = {
     new QueryGenerator(schema).generateQuery(query).right.get
   }
